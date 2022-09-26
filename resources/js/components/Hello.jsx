a075fb4a6148e89532b97ab4832f1018f505b213
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-09-26 19:43:31 +0900

    api仮作成

--- resources/js/components/Hello.jsx
+++ resources/js/components/Hello.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react"
 export const Hello = () => {
+    useEffect(() => {
+        fetch('/api/hello')
+            .then((res) => console.log(res.json))
+            // .then((data) => console.log(data))
+            .catch((e) => console.error(e))
+    }, []);
     return (
         <div>
             <h2>Hello components</h2>

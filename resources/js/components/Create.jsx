fee5f23454a6ae42e565c34970d5ba25382d06b4
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-09-28 19:47:12 +0900

    form作成

--- resources/js/components/Create.jsx
+++ resources/js/components/Create.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
-import { Button } from '@mui/material';
+import { Box, Button, Container, TextField } from '@mui/material';
+import { MainTable } from './MainTable';
+import { borderRadius } from "@mui/system";
 
 export const Create = () => {
     // useEffect(() => {
@@ -10,16 +12,38 @@ export const Create = () => {
     // }, []);
     return (
         <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Create!?</div>
-                        <div className="card-body">Im an example component</div>
-                        <Button color="primary" variant="contained">Hello World</Button>
-                        <Button color="secondary" variant="contained">Hello World</Button>
-                    </div>
-                </div>
+            <h1>新規登録</h1>
+            <div>
+                <Container maxWidth="lg">
+                    <Box sx={{
+                        margin: "40px auto",
+                        padding: "32px",
+                        width: "600px",
+                        border: "1px solid #f7f7f7",
+                        borderRadius: "4px",
+                        boxShadow: "4px 4px 15px rgba(0,0,0,0.1)"
+                    }}>
+                        <form action="">
+                            <TextField
+                                // error
+                                id="name"
+                                label="name"
+                                placeholder="Hello World!"
+                                variant="outlined"
+                            />
+                            <TextField
+                                sx={{ marginLeft: "24px" }}
+                                id="task"
+                                label="task"
+                                placeholder="タスクを入力"
+                                variant="outlined"
+                            />
+                            <Button sx={{ marginLeft: "16px", marginTop: "8px" }} variant="contained">登録！</Button>
+                        </form>
+                    </Box>
+                </Container>
             </div>
+            <MainTable />
         </div>
     )
 }

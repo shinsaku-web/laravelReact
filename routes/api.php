8b57e4a00e54914b9b9cb8c5964a7df11f865a30
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-09-29 18:54:55 +0900

    ルートの定義

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,11 @@ Route::get('/hello', function () {
     // return json_encode(["a" => "hello", "b" => "React", "c" => "Laravel"]);
     return json_encode(["hello", "React", "Laravel"]);
 });
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'delete'])->name('tasks.destroy');
